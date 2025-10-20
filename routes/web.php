ab7824dab5bec9d4e57a7f5d1fa88970cba7ab7a
sharamomid1399-art <sharam.omid1399@gmail.com>
2025-10-20 21:25:30 +0430

    i changed the welcome page

--- routes/web.php
+++ routes/web.php
@@ -5,34 +5,9 @@ use Illuminate\Support\Facades\Route;
 // get -> for show a page
 
 Route::get('/', function () {
-    return view('Home');
-});
-
-Route::get('about' , function(){
-    return view('About');
-});
-
-Route::get('contact' , function(){
-    return view('Contact');
+    // return view('Home');
 });
 
 
 
 
-
-
-
-// post -> for saving data
-Route::post('/save' , function(){
-
-});
-
-// put -> for Updating data
-Route::put('/updating' , function(){
-
-});
-
-// delete -> for deleting data
-Route::delete('/delete', function(){
-
-});
